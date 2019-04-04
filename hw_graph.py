2e998c411ed247bdcd82d1b0f949812aaf779502
VincentCollins <1035727898@qq.com>
2019-04-04 19:23:43 +0800

    complete edition

---
+++ hw_graph.py
@@ -0,0 +1,171 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from dataAnalysis.hw_spider import *
+import os
+import json
+class graphing():
+    def __init__(self):
+        self.data = {'OVERALL':{},'FOOD':{},'CLOTHING':{},'ACCOMODATION':{},'TRANSPORTATION':{},'EDU_ENTERTAINMENT':{},
+                     'TOTALPOP':{},'MALEPOP':{},'FEMALEPOP':{},
+                     'POP_0to14':{},'POP_15to64':{},'POP_65plus':{},
+                     'GDP':{},'ASCEND_PI':{},'ASCEND_SI':{},'ASCEND_TI':{}}
+
+
+    def spider_and_save(self):
+        self.Spider = my_spider()
+        self.Spider.main_func()#到此为止已经在数据库中存入所需数据
+        pass
+
+    def load_database(self,database):
+        self.my_sq = sqlite3.connect(database)
+        self.cursor = self.my_sq.cursor()
+        for self.tag in self.data:
+            self.items = self.cursor.execute("SELECT year,value from %s" % self.tag)
+            for self.item in self.items:
+                self.data[self.tag][str(self.item[0])] = self.item[1]
+
+    def create_axis(self,tag):
+        value = []
+        year = []
+        for i, item in enumerate(self.data[tag]):
+            year.append(int(item))
+            value.append(self.data[tag][item])
+        return np.array(year),np.array(value)
+
+    def graph0(self):
+        # 2000年农村人均消费关于消费类型的分布 饼状图
+        labels = ['FOOD','CLOTHING','ACCOMODATION','TRANSPORTATION','EDU_ENTERTAINMENT','OTHERS']
+        sizes = []
+        explode = [0]
+        for i in labels[0:-1]:
+            sizes.append(self.data[i]['2000']/self.data['OVERALL']['2000'])
+            explode.append(0)
+        sizes.append(1-sum(sizes))
+        plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=150)
+        plt.title('Cost in diffrent aspects of rural population in 2000')
+        plt.savefig('fig7.png', bbox_inches='tight')
+        plt.show()
+
+        # 各年龄段占比变化图
+        x, y1 = self.create_axis('FOOD')  # 长度跟其他三个不一样
+        x, y2 = self.create_axis('CLOTHING')
+        x, y3 = self.create_axis('ACCOMODATION')
+        x, y4 = self.create_axis('TRANSPORTATION')
+        plt.plot(x, y1, label='Food')
+        plt.plot(x, y2, label='Clothing')
+        plt.plot(x, y3, label='Accomodation')
+        plt.plot(x, y4, label='Transportation')
+        plt.xticks(np.arange(1999, 2013, 1), rotation=60)
+        plt.xlabel('Graph. Average cost in four main aspects in rural area vs Year', fontsize=14)
+        plt.ylabel('Cost/yuan',fontsize=12)
+        plt.legend(loc='upper left')
+        plt.savefig('fig8.png', bbox_inches='tight')
+        plt.show()
+
+
+
+    def graph2(self):
+        num0 = self.data['TOTALPOP']['2016']
+        num1 = self.data['POP_0to14']['2016']
+        num2 = self.data['POP_15to64']['2016']
+        num3 = self.data['POP_65plus']['2016']
+
+        #2016年各年龄段占比 饼状图
+        labels = ['0~14 years old','15~64 years old','over 65 years old']
+        sizes = [num1/num0,num2/num0,num3/num0]
+        explode = [0,0.1,0]
+        plt.pie(sizes, explode=explode,labels= labels,autopct='%1.1f%%',shadow=False,startangle=150)
+        plt.title('population propotion for three ranges of age in 2016')
+        plt.savefig('fig3.png', bbox_inches='tight')
+        plt.show()
+
+        #各年龄段占比变化图
+        x, y = self.create_axis('TOTALPOP')#长度跟其他三个不一样
+        x, y1 = self.create_axis('POP_0to14')
+        x, y2 = self.create_axis('POP_15to64')
+        x, y3 = self.create_axis('POP_65plus')
+        y1=np.divide(y1,y[0:-1])
+        y2=np.divide(y2,y[0:-1])
+        y3=np.divide(y3,y[0:-1])
+        plt.plot(x, y1, label='0~14(age)')
+        plt.plot(x, y2, label='15~64(age)')
+        plt.plot(x, y3, label='>65(age)')
+        plt.xticks(np.arange(1999, 2018, 1), rotation=60)
+        plt.xlabel('Graph. population propotion of three ranges of age vs Year', fontsize=14)
+        plt.ylabel('Population propotion')
+        plt.legend(loc='center right')
+        plt.savefig('fig4.png', bbox_inches='tight')
+        plt.show()
+
+    def graph3(self):
+        #GDP变化折线图
+        x, y = self.create_axis('GDP')
+        plt.plot(x, y, label='GDP')
+        plt.xticks(np.arange(1999, 2019, 1), rotation=60)
+        plt.xlabel('Graph. GDP vs Year', fontsize=14)
+        plt.ylabel('GDP/10^8')
+        plt.legend(loc='center left')
+        plt.savefig('fig5.png', bbox_inches='tight')
+        plt.show()
+
+        #三类产业逐年变化条形图
+        x, y1 = self.create_axis('ASCEND_PI')
+        x, y2 = self.create_axis('ASCEND_SI')
+        x, y3 = self.create_axis('ASCEND_TI')
+        plt.bar(x[0:10], y1[0:10], align='edge',width=0.2,label='Primary     industry')
+        plt.bar(np.add(x,0.2)[0:10], y2[0:10], align='edge',width=0.2,label='Secondary industry')
+        plt.bar(np.add(x,0.4)[0:10], y3[0:10], align='edge',width=0.2,label='Tertiary     industry')
+        plt.xticks(np.arange(2009, 2019, 1), rotation=60)
+        plt.yticks(np.arange(0, 490000, 50000))
+        plt.xlabel('Graph. Ascendence of three types of industry vs Year', fontsize=14)
+        plt.ylabel('Ascendence/10^8')
+        plt.legend(loc='upper left')
+        plt.savefig('fig6.png', bbox_inches='tight')
+        plt.show()
+
+    def graph1(self):
+        # 总人口条形图
+        x,y = self.create_axis('TOTALPOP')
+        plt.bar(x,y,align='edge',width=0.4,color='gold',edgecolor='mediumblue',lw=3)
+        plt.xticks(np.arange(1999,2019,1),rotation=60)
+        plt.yticks(np.arange(0,140000,5000))
+        plt.xlabel('Graph. Total population vs Year',fontsize=14)
+        plt.ylabel('Total population/10^4')
+        plt.savefig('fig1.png',bbox_inches = 'tight')
+        plt.show()
+
+        #男女性总人口条形图
+        x, y1 = self.create_axis('MALEPOP')
+        x, y2 = self.create_axis('FEMALEPOP')
+        plt.plot(x, y1,label='MALE')
+        plt.plot(x, y2, label='FEMALE')
+        plt.xticks(np.arange(1999,2019,1),rotation=60)
+        plt.xlabel('Graph. Total population of F&M vs Year', fontsize=14)
+        plt.ylabel('Total population/10^4')
+        plt.legend(loc='center left')
+        plt.savefig('fig2.png', bbox_inches='tight')
+        plt.show()
+
+
+
+
+if __name__ =="__main__":
+    Graph = graphing()
+    if not os.path.exists('myjson.json'):
+        Graph.spider_and_save()
+        Graph.load_database('mydb.db')
+        with open('myjson.json','w') as load_f:
+            json.dump(Graph.data,load_f)
+        load_f.close()
+
+    else:
+        with open('myjson.json','r') as load_f:
+            Graph.data = json.load(load_f)
+        load_f.close()
+
+    # for item in Graph.data:
+    #     print(Graph.data[item])
+    Graph.graph0()
+    Graph.graph1()
+    Graph.graph2()
+    Graph.graph3()
---
+++ hw_spider.py
@@ -0,0 +1,121 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import sqlite3
+import os
+
+class my_spider():
+    def __init__(self):
+        pass
+
+    def html_process(self,html_text,database,tablename,maindata_type,line,end_year):
+        '''line从0开始'''
+        # 连接数据库
+        self.my_sq = sqlite3.connect(database)
+        self.cursor = self.my_sq.cursor()
+
+        # 处理html文本
+        self.bs = BeautifulSoup(html_text, 'lxml')
+        self.rows = self.bs.select('table.public_table.table_main tbody tr')  # 获取各行的人口数据
+        # 获得的list中的成员就是bs4.element.Tag类型
+        self.index_row = self.bs.select('table.public_table.table_main thead tr')  # 虽然只有一行，但仍然装在list里面
+
+        #print('b1')
+        # 写入数据库
+        try:
+            self.cursor.execute('''DROP TABLE if exists %s'''%tablename)
+            self.cursor.execute('''CREATE TABLE %s
+                                        (YEAR   INT PRIMARY KEY NOT NULL,
+                                         VALUE  %s              NOT NULL);
+                                        '''%(tablename,maindata_type))
+        except:
+            pass
+        #print('b2')
+        for self.i, self.td in enumerate(self.rows[line].contents[1:]):  # 遍历第一行数据，跳过第一行第一个表头数据
+            #print('b')
+            self.year = int(self.index_row[0].contents[self.i + 1].get_text()[0:4])  # 一个年份数据
+            if self.year>end_year:#有必要的话，跳过前面的数据（如跳过2018年）
+                continue
+            if maindata_type=='int':
+                self.value = int(self.td.get_text())  # 一个整型数据
+                self.cursor.execute('''INSERT INTO %s(YEAR, VALUE) VALUES('%d','%d')''' % (tablename,self.year, self.value))
+            else:
+                self.value = float(self.td.get_text())  # 一个浮点型数据
+                self.cursor.execute(
+                    '''INSERT INTO %s(YEAR, VALUE) VALUES('%d','%f')''' % (tablename, self.year, self.value))
+        self.my_sq.commit()
+        self.my_sq.close()
+
+    def table_print(self,tablename):
+        self.my_sq = sqlite3.connect('mydb.db')
+        self.cursor = self.my_sq.cursor()
+        self.items = self.cursor.execute("SELECT YEAR,VALUE from %s"%tablename)
+
+        for self.item in self.items:
+            print("YEAR: ",self.item[0],type(self.item[0]))
+            print("VALUE: ",self.item[1],type(self.item[1]),'\n')
+
+    def task0(self,html_text):
+        self.html_process(html_text, 'mydb.db', 'OVERALL', 'float', 0, 2012)
+        self.html_process(html_text, 'mydb.db', 'FOOD', 'float', 1, 2012)
+        self.html_process(html_text, 'mydb.db', 'CLOTHING', 'float', 2, 2012)
+        self.html_process(html_text, 'mydb.db', 'ACCOMODATION', 'float', 3, 2012)
+        self.html_process(html_text, 'mydb.db', 'TRANSPORTATION', 'float', 5, 2012)
+        self.html_process(html_text, 'mydb.db', 'EDU_ENTERTAINMENT', 'float', 6, 2012)
+
+    def task1(self,html_text):#处理三个总人口数据
+        self.html_process(html_text,'mydb.db','TOTALPOP','int',0,2018)
+        self.html_process(html_text, 'mydb.db', 'MALEPOP', 'int', 1,2018)
+        self.html_process(html_text, 'mydb.db', 'FEMALEPOP', 'int', 2,2018)
+        #self.table_print('TOTALPOP')
+
+    def task2(self,html_text):#处理年龄分布数据
+        self.html_process(html_text, 'mydb.db', 'POP_0to14', 'int', 1,2017)
+        self.html_process(html_text, 'mydb.db', 'POP_15to64', 'int', 2,2017)
+        self.html_process(html_text, 'mydb.db', 'POP_65plus', 'int', 3,2017)
+
+    def task3(self,html_text):
+        self.html_process(html_text, 'mydb.db', 'GDP', 'float', 1, 2018)
+        self.html_process(html_text, 'mydb.db', 'ASCEND_PI', 'float', 2, 2018)
+        self.html_process(html_text, 'mydb.db', 'ASCEND_SI', 'float', 3, 2018)
+        self.html_process(html_text, 'mydb.db', 'ASCEND_TI', 'float', 4, 2018)
+
+
+
+    def spider_and_get_HTML(self,html_filename,branch1,branch2):
+        if not os.path.exists('C:\\Users\Vincent Collins\\git_rep\\dataAnalysis\\%s'%html_filename):
+            self.browser = webdriver.Chrome()  # 调用本地的Chrome浏览器
+                                               #注意，这一行命令会打开浏览器！
+            self.browser.get('http://data.stats.gov.cn/easyquery.htm?cn=C01')  # 请求页面，会打开一个浏览器窗口
+            time.sleep(3)
+            self.browser.find_element_by_xpath("//span[@id='treeZhiBiao_%d_span']"%branch1).click()
+            time.sleep(3)
+            self.browser.find_element_by_xpath("//span[@id='treeZhiBiao_%d_span']"%branch2).click()
+            time.sleep(3)
+            self.browser.find_element_by_xpath("//*[@class='dtHead'][@title='最近10年']").click()
+            time.sleep(3)
+            self.browser.find_element_by_xpath("//li[@title='最近20年']").click()
+            time.sleep(3)
+
+            self.myfile = open(html_filename, 'w')
+            self.myfile.write(self.browser.page_source)
+            self.myfile.close()
+
+        self.myfile = open(html_filename,'r')
+        return self.myfile.read()
+
+    def main_func(self):
+        self.html_text = self.spider_and_get_HTML('html0.txt', 11, 46)
+        self.task0(self.html_text)
+        self.html_text = self.spider_and_get_HTML('html1.txt',4,30)
+        self.task1(self.html_text)
+        self.html_text = self.spider_and_get_HTML('html2.txt', 4, 32)
+        self.task2(self.html_text)
+        self.html_text = self.spider_and_get_HTML('html3.txt', 3, 30)
+        self.task3(self.html_text)
+
+if __name__ =='__main__':
+    Spider = my_spider()
+    Spider.main_func()
+
